f221511e32feb6e82d8fca356aebbe8b072d7e5d
dani-isabel <59672017+dani-isabel@users.noreply.github.com>
2020-06-14 09:53:10 -0500

    delete users and exist user middleware

--- server.js
+++ server.js
@@ -5,35 +5,44 @@ const app = express();
 app.use(express.json());
 //CRUD users
 //Get query users
-app.get("/users",(req,res) => {
+app.get("/users", (req, res) => {
     const query = "SELECT * FROM users";
-    dataBase.query(query, {type:sequelize.QueryTypes.SELECT})
+    dataBase.query(query, { type: sequelize.QueryTypes.SELECT })
         .then((users) => {
             res.json(users);
         })
         .catch((e) => console.log(e));
 })
 //Post query users
-app.post("/users",(req,res) => {
+app.post("/users", (req, res) => {
     const query = "INSERT INTO users (id,username,email,name,phone,password,address) VALUES (?,?,?,?,?,?,?)";
-    const {id,username,email,name,phone,password,address} = req.body;
-    dataBase.query(query, {replacements: [id,username,email,name,phone,password,address]})
-    .then((response) => {
-        res.json({status:"insertado",usuario:req.body});
-    }).catch((e) => console.error(e));
+    const { id, username, email, name, phone, password, address } = req.body;
+    dataBase.query(query, { replacements: [id, username, email, name, phone, password, address] })
+        .then((response) => {
+            res.json({ status: "insertado", usuario: req.body });
+        }).catch((e) => console.error(e));
 })
 //middleware exist
-const userExist = (req,res,next) => {
-    const name = res.query.name;
-    const exist = "SELECT * FROM users WHERE name = ?";
-    dataBase.query(exist, {replacements: [name],type:sequelize.QueryTypes.SELECT})
-    .then(data => {
-        if(!data.legth) {
-            return res.status(404).json({error: "Usuario no registrado"})
-        }
-        return next();
-    }).catch(e => {
-        return res.status(404).json({error: "Algo salio mal..."})
-    })
+const userExist = (req, res, next) => {
+    const username = req.query.username;
+    const exist = "SELECT * FROM users WHERE username = ?";
+    dataBase.query(exist, { replacements: [username], type: sequelize.QueryTypes.SELECT })
+        .then(data => {
+            if (!data.length) {
+                return res.status(404).json({ error: "User is not register" })
+            }
+            return next();
+        }).catch(e => {
+            return res.status(404).json({ error: "Something went wrong...",e })
+        })
 }
-app.listen(4000, (res,req) => console.log("Escuchando por el puerto 4000"));
+//Delete users
+app.delete("/users",userExist, (req, res) => {
+    const username = req.query.username;
+    const query = "DELETE FROM users WHERE username = ?";
+    dataBase.query(query, { replacements: [username] })
+        .then ((data) => {
+            res.json({status: "User delete"});
+        }).catch(e => console.log("Something went wrong...",e));
+});
+app.listen(4000, (res, req) => console.log("Listening in the port 4000"));
