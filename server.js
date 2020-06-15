fea52430e4f4dff9ce52cd5f13ca3cfb605a4e77
dani-isabel <59672017+dani-isabel@users.noreply.github.com>
2020-06-14 20:55:24 -0500

    complete CRUD of users and dishes

--- server.js
+++ server.js
@@ -19,10 +19,10 @@ app.post("/users", (req, res) => {
     const { id, username, email, name, phone, password, address } = req.body;
     dataBase.query(query, { replacements: [id, username, email, name, phone, password, address] })
         .then((response) => {
-            res.json({ status: "insertado", usuario: req.body });
+            res.json({ status: "User created", user: req.body });
         }).catch((e) => console.error(e));
 })
-//middleware exist
+//Middleware exist user
 const userExist = (req, res, next) => {
     const username = req.query.username;
     const exist = "SELECT * FROM users WHERE username = ?";
@@ -33,16 +33,76 @@ const userExist = (req, res, next) => {
             }
             return next();
         }).catch(e => {
-            return res.status(404).json({ error: "Something went wrong...",e })
+            return res.status(404).json({ error: "Something went wrong...", e })
         })
 }
 //Delete users
-app.delete("/users",userExist, (req, res) => {
+app.delete("/users", userExist, (req, res) => {
     const username = req.query.username;
     const query = "DELETE FROM users WHERE username = ?";
     dataBase.query(query, { replacements: [username] })
-        .then ((data) => {
-            res.json({status: "User delete"});
-        }).catch(e => console.log("Something went wrong...",e));
+        .then((data) => {
+            res.json({ status: "User delete" });
+        }).catch(e => console.log("Something went wrong...", e));
 });
+//Put new existing users info
+app.put("/users", userExist, (req, res) => {
+    const username = req.query.username;
+    const query = "UPDATE users SET email = ?,phone = ?,password = ?,address = ? WHERE username = ?";
+    const { email, phone, password, address } = req.body;
+    dataBase.query(query, { replacements: [email, phone, password, address, username] })
+        .then((response) => {
+            res.json({ status: "User updated successful" });
+        }).catch((e) => console.error(e));
+})
+//Get dishes
+app.get("/dishes", (req, res) => {
+    const dishes = "SELECT * FROM dishes";
+    dataBase.query(dishes, { type: sequelize.QueryTypes.SELECT })
+        .then((dishes) => {
+            res.json(dishes);
+        }).catch((e) => console.log(e));
+})
+//Post new dishes
+app.post("/dishes", (req, res) => {
+    const query = "INSERT INTO dishes (id,name,price) VALUES (?,?,?)";
+    const { id, name, price } = req.body;
+    dataBase.query(query, { replacements: [id, name, price] })
+        .then ((response) => {
+            res.json({status: "Dish created", user: req.body });
+        }). catch((e) => console.log(e));
+})
+//Middleware exist dish
+const dishExist = (req,res,next) => {
+    const id = req.query.id;
+    const existDish = "SELECT * FROM dishes WHERE id = ?";
+    dataBase.query(existDish, {replacements: [id], type: sequelize.QueryTypes.SELECT})
+        .then(data => {
+            if (!data.length) {
+                return res.status(404).json({dish: "Dish is not register"});
+            }
+            return next();
+        }).catch(e => {
+            return res.status(404).json({error: "Something went wrong..."}) 
+        })
+}
+//Delete dish
+app.delete("/dishes",dishExist, (req,res) => {
+    const id = req.query.id;
+    const query = "DELETE FROM dishes WHERE id = ? ";
+    dataBase.query(query, {replacements: [id]})
+        .then ((data) => {
+            res.status(404).json({ status: "Dish delete"});
+        }).catch(e => console.log("Something went wrong ...", (e)));
+})
+//Edit existing dishes
+app.put("/dishes", dishExist, (req,res) => {
+    const id = req.query.id;
+    const query = "UPDATE dishes SET name = ?, price = ? WHERE id = ?";
+    const {name,price} = req.body;
+    dataBase.query(query, {replacements: [name,price,id]})
+    .then ((response) => {
+        res.json({status: "Dish update successful"});
+    }).catch((e) => console.error(e))
+})
 app.listen(4000, (res, req) => console.log("Listening in the port 4000"));
