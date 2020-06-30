edaf27d0a00b2c70abef4e3e7c10c074e4b1f39e
dani-isabel <59672017+dani-isabel@users.noreply.github.com>
2020-06-30 18:48:43 -0500

    insert order with array of products and default time

---
+++ middlewares/validateOrder.js
@@ -0,0 +1,28 @@
+const sequelize = require("sequelize");
+const dataBase = new sequelize("mysql://root:@localhost:3306/delilahresto");
+
+const insertNew = (req,res,next) => {
+    const query = "INSERT INTO orders (code_status,id_paymethod) VALUES (1,?)";
+    const {id_paymethod} = req.body;
+    dataBase.query(query, {replacements: [id_paymethod]})
+        .then((data) => {
+            console.log(data[0]);
+            const id_order = data[0];
+            req.id_order = id_order;
+            next();
+        }).catch((e) => console.log(e));
+}
+
+const insertDishes = (req,res,next) => {
+    const {dishes} = req.body;
+    const {id_order} = req;
+    for (i in dishes) {
+        const query = "INSERT INTO orders_description (id_order,id_dishes) VALUES (?,?)";
+        dataBase.query(query, {replacements: [id_order,dishes[i].id_dishes]})
+        .then((data) => {
+            console.log(data);
+            next();
+        }).catch((e) => console.log(e))
+    }
+}
+module.exports = {insertDishes, insertNew}
